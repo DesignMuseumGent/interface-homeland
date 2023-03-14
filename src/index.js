26ac38b52c2fd457b9e58345997c24c9f411b8e1
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-03-14 20:32:19 +0100

    add other screens

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import{BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Screen2text from "./screens/Screen_2_text";
 import Screen1images from "./screens/Screen_1_images";
-import Screen_3_images from "./screens/Screen_3_images";
+import Screen3images from "./screens/Screen_3_images";
 import Screen4text from "./screens/Screen_4_text";
 
 
