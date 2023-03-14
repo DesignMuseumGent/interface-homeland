6fa6bcef384fd90901249909032e2b50ea642b66
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-03-14 15:33:03 +0100

    full screen

--- src/utils/TextBlock.js
+++ src/utils/TextBlock.js
@@ -6,7 +6,7 @@ const TextBlock = (props) => {
     const _story = props.story;
 
     return(
-        <div>
+        <div style={{height: "70vh"}}>
             <h1 className={"title"} style={{textAlign:"center", padding: "1vw"}}>{texts[_story][_lang]["title"]}</h1>
             <div className={"textContainer"}>
                 <p style={{textAlign: "justify", padding: "4vw"}} className={"text"}>{texts[_story][_lang]["text"]}</p>
