ec3c2f6177b0c19572a948aeebc2f80275f2b967
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-03-17 10:12:30 +0100

    add other screens

--- src/screens/Screen_1_images.js
+++ src/screens/Screen_1_images.js
@@ -8,9 +8,10 @@ const Screen1images = (props) =>  {
     const [story, setStory] = useState(0)
 
     useInterval(function(){
+        let _len = story.length() -1 ;
         let sec = new Date().getSeconds()
         if (sec === 20) {
-            if (count < 4) {
+            if (count < _len) {
                 setCount(count+1)
                 setStory(count)
                 console.log(count);
