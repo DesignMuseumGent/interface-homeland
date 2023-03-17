382658666767f68017edd3f397ec345f6a89ac18
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-03-17 09:36:06 +0100

    add other screens

--- src/utils/TextBlock.js
+++ src/utils/TextBlock.js
@@ -10,7 +10,7 @@ const TextBlock = (props) => {
         <div>
             <h1 className={"title Hline"} style={{textAlign:"center", padding: "1vw"}}>{texts[_story][_lang]["title"]}</h1>
             <div className={"textContainer"}>
-                <p style={{textAlign: "justify", padding: "4vw"}} className={font?"textSmall":"text"}>{texts[_story][_lang]["text"]}</p>
+                <p style={{padding: "4vw"}} className={font?"textSmall":"text"}>{texts[_story][_lang]["text"]}</p>
             </div>
         </div>
     )
